9aac6040f2ffa62faba015610d43d6e5a8d8cea8
Dicky <Dicky.Qi@icloud.com>
2022-01-23 16:38:06 +0800

    update SDK version to 1.6.2

--- include/BluePrint.h
+++ include/BluePrint.h
@@ -424,7 +424,7 @@ private:
 # define VERSION_BUILT(v)   (v&0x000000FF)
 # define MAJOR  1
 # define MINOR  6
-# define PATCH  1
+# define PATCH  2
 # define BUILT  0
 
 // Please Set CMakeList.txt IMGUI_BP_SDK_VERSION_STRING also
